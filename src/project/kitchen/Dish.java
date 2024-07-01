7ab79426ac27d8a92579019af59b8b94fcf3420e
Vyacheslav-Voloshin <oks.volosh1na1981@gmail.com>
2024-07-01 20:09:53 +0300

    Create method static String allDishesToString()

--- src/project/kitchen/Dish.java
+++ src/project/kitchen/Dish.java
@@ -6,4 +6,8 @@ public enum Dish {
     SOUP,
     JUICE,
     WATER;
+//Метод, який відображає для користувача страви
+    public static String allDishesToString(){
+        return "FISH, STEAK, SOUP, JUICE, WATER";
+    }
 }
