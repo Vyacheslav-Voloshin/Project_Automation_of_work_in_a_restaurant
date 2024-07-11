adce24894c9abbb56c14dc0533525a748d3e1166
Vyacheslav-Voloshin <oks.volosh1na1981@gmail.com>
2024-07-11 23:39:04 +0300

    Create logic for methods: printAdvertisementProfit(); printCookWorkloading();   in class DirectorTablet

--- src/project/DirectorTablet.java
+++ src/project/DirectorTablet.java
@@ -1,4 +1,10 @@
 package project;
+
+import project.statistic.StatisticManager;
+import project.statistic.event.EventDataRow;
+
+import java.util.*;
+
 /*
  Директор має з якогось місця викликати ці методи.
 Дамо йому планшет, але з іншим програмним забезпеченням.
@@ -9,10 +15,34 @@ package project;
 public class DirectorTablet {
 
     //метод вираховуючий, яку суму заробили на рекламі, згрупувати днями;
-    public void printAdvertisementProfit(){}
+    public void printAdvertisementProfit(){
+        StatisticManager statisticManager = StatisticManager.getInstance();
+        Map<String,Long> map = statisticManager.profitForEveryDay();
+        ArrayList<String> arrayList = new ArrayList(map.keySet());
+        Collections.sort(arrayList);
+        for (String key:arrayList) {
+            double number = 1.0* map.get(key)/100;
+            System.out.println(key+" - " + String.format(Locale.ENGLISH,"%.2f",number));
+        }
+    }
 
     // метод вираховуючий, завантаження (робочий час) кухаря, згрупувати по днях;
-    public void printCookWorkloading(){}
+    public void printCookWorkloading(){
+        StatisticManager statisticManager = StatisticManager.getInstance();
+        Map<String,Map<String,Integer>> map = statisticManager.getCookWorkloadingMap();
+        ArrayList<String> arrayList = new ArrayList(map.keySet());
+        Collections.sort(arrayList);
+        for (String date:arrayList) {
+            Map<String,Integer> map1 = map.get(date);
+            System.out.println(date);
+            ArrayList<String>arrayList1 = new ArrayList<>(map1.keySet());
+            Collections.sort(arrayList1);
+            for (String name:arrayList1) {
+                System.out.println(name + " - " + ((map1.get(name)+59)/60) + " min");
+            }
+        }
+        System.out.println();
+    }
 
     //список активних роликів і кількість показів, що залишилася, по кожному;
     public void printActiveVideoSet(){}
